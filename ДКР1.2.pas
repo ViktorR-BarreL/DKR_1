5b15e01eff11327fb79e338e5da7521374999f27
ViktorR-BarreL <144677749+ViktorR-BarreL@users.noreply.github.com>
2023-10-10 15:04:29 +0300

    Add files via upload

---
+++ ДКР1.2.pas
@@ -0,0 +1,17 @@
+﻿program system_02;
+
+var
+  x, y: real;
+
+begin
+  x := -11;
+  while x < 7.3 do
+  begin
+    if (x < -9) then y := ln(x);
+    if (x < -4) then y := (x ** (0.1 * x)) / (x ** 3 - x);
+    if (x < 5) then y := sqr(x) / 21 - 23 / 29;
+    if (x >= 5) then y := x ** (0.1 * x) * 2 - x ** (0.1 * x);
+    writeln('x= ', x:0:1, '     ', 'y= ', y:0:2);
+    x := x + 0.3;
+  end;
+end.
---
+++ ДКР1.pas
@@ -0,0 +1,14 @@
+﻿program system_01;
+
+var
+  x, y: real;
+
+begin
+  write('Введите х:  ');
+  readln(x);
+  if (x < -9) then y := ln(x);
+  if (x < -4) then y := (x ** (0.1 * x)) / (x ** 3 - x);
+  if (x < 5) then y := sqr(x) / 21 - 23 / 29;
+  if (x >= 5) then y := x ** (0.1 * x) * 2 - x ** (0.1 * x);
+  writeln('y=', y:0:2);
+end.
